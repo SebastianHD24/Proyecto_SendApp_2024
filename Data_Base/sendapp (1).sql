951e72eb77f2fad5e101338f8815f91c7a7c2590
TheSebasHD24 <116607670+SebastianHD24@users.noreply.github.com>
2024-03-07 10:44:47 -0500

    Base de datos

---
+++ Data_Base/sendapp (1).sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-03-2024 a las 14:34:25
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sendapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id_cita` int(11) NOT NULL COMMENT 'Identifica la id de la cita',
+  `estado_cita` tinyint(1) NOT NULL COMMENT 'Muestra el estado de la cita',
+  `fecha` date NOT NULL COMMENT 'Muestra la fecha',
+  `hora` datetime NOT NULL COMMENT 'Muestra la hora',
+  `documento_usuario` int(12) NOT NULL COMMENT 'Muestra la TI o CC',
+  `id_servicio` int(2) NOT NULL COMMENT 'Muestra la id de los servicios'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(2) NOT NULL COMMENT 'Visualiza el rol',
+  `nombre_rol` char(20) NOT NULL COMMENT 'Muestra el nombre del rol',
+  `estado_rol` tinyint(1) NOT NULL COMMENT 'Muestra el estado del rol'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre_rol`, `estado_rol`) VALUES
+(1, 'administrador', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id_servicio` int(2) NOT NULL COMMENT 'Identifica los servicos del sena',
+  `nombre_servicio` char(40) NOT NULL COMMENT 'Es el  nombre de los servicios',
+  `admin_area` char(40) NOT NULL COMMENT 'Identifica el area',
+  `estado_servicio` tinyint(1) NOT NULL COMMENT 'Muestra el estado de los servicios'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `documento_identidad` int(12) NOT NULL COMMENT 'Es la identidad de la persona',
+  `contrasena` varchar(10) NOT NULL COMMENT 'Es la contrasena de todos los usuarios',
+  `nombres` varchar(30) NOT NULL COMMENT 'Nombre del usuario\r\n',
+  `apellidos` varchar(30) NOT NULL COMMENT 'Apellido del usuario',
+  `correo` varchar(30) NOT NULL COMMENT 'Correo del usuario',
+  `celular` int(15) NOT NULL COMMENT 'Celular del usuario',
+  `programa` varchar(50) NOT NULL COMMENT 'Programa del usuario',
+  `ficha` int(10) DEFAULT NULL COMMENT 'Ficha del usuario',
+  `estado` tinyint(1) NOT NULL COMMENT 'Estado del usuario',
+  `id_rol` int(2) DEFAULT NULL COMMENT 'Rol del usuario',
+  `id_servicio` int(2) DEFAULT NULL COMMENT 'Los servicios del ususario'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id_cita`),
+  ADD KEY `id_servicio` (`id_servicio`),
+  ADD KEY `documento_usuario` (`documento_usuario`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`documento_identidad`),
+  ADD KEY `id_servicio` (`id_servicio`),
+  ADD KEY `id_servicio_2` (`id_servicio`),
+  ADD KEY `id_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifica la id de la cita';
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(2) NOT NULL AUTO_INCREMENT COMMENT 'Visualiza el rol', AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id_servicio` int(2) NOT NULL AUTO_INCREMENT COMMENT 'Identifica los servicos del sena';
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`documento_usuario`) REFERENCES `usuarios` (`documento_identidad`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `citas_ibfk_3` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
