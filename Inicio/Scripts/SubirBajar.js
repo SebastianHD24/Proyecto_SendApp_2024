2be50da8b143fbef198d381e38f7d3b5bf7dcfa5
TheSebasHD24 <116607670+SebastianHD24@users.noreply.github.com>
2024-03-28 15:27:56 -0500

    Delete Inicio directory

--- Inicio/Scripts/SubirBajar.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('label_toggle4').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-// Bajar al final de la página
-document.getElementById('label_toggle5').addEventListener('click', function() {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-});
