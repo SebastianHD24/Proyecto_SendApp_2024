de6aa205fabb6b95b49d20b82d317d49e8cc35f2
Santiago García <santy.g18@hotmail.com>
2024-05-02 09:10:57 -0500

    Se agregaron los scripts y la alerta de confirmación

---
+++ componentes/formulario/script/formulario-editar.js
@@ -0,0 +1,21 @@
+// FORMULARIOS
+contraseña = document.getElementById('cambiarContraseña');
+mainForm = document.getElementById('formularioPrincipal');
+alerta = document.getElementById('alerta');
+//BOTOBES
+botonContraseña = document.getElementById('btnCambiar');
+botonConfirmar = document.getElementById('btnConfirmarcontra');
+//EVENTOS
+botonContraseña.addEventListener('click', function(){
+    contraseña.style.display = 'block';
+    mainForm.style.display = 'none';
+});
+botonConfirmar.addEventListener('click',function(){
+    contraseña.style.display = 'none';
+    mainForm.style.display = 'none';
+    alerta.style.display = 'block';
+    setTimeout(function(){
+        alerta.style.display = 'none';
+        mainForm.style.display = 'block';
+    }, 3000);
+})
