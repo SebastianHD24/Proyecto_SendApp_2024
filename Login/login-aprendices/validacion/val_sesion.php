42bceda46fead0236966ba3b6a57e280e12e5bb2
Herllei <g2js06022003@gmail.com>
2024-05-04 14:47:47 -0500

    cambio del ajax al fech y cambio en el html del login con los id, poniendo una letra diferente a cada id

--- Login/login-aprendices/validacion/val_sesion.php
+++ Login/login-aprendices/validacion/val_sesion.php
@@ -3,8 +3,8 @@
 
     // Verificar si el usuario ha iniciado sesión
     if (!isset($_SESSION["documento_identidad"])) {
-        // Si no ha iniciado sesión, redirigirlo a la página de inicio de sesión
-        echo json_encode(array('success' => 2)); // Asegurarse de que el script se detenga después de la redirección
+        // Si no ha iniciado sesión, redirigir a la página de inicio de sesión
+        echo json_encode(array('success' => 2)); // Asegurarse de que el script se detenga después de la direccion
     } else {
         echo json_encode(array('success' => 1));
     }
