26dcb0193a6b7798ad5c7966fb1b97a301a7fa6a
Herllei <g2js06022003@gmail.com>
2024-05-04 12:46:39 -0500

    Mejora en el val.php que tenia unos errores, y js que esta asociado a ese php

--- Login/login-aprendices/validacion/val.php
+++ Login/login-aprendices/validacion/val.php
@@ -1,52 +1,70 @@
 <?php
     error_reporting(0); 
     session_start();
-
-    include("../../../../Proyecto_SendApp_2024/bases/conexion.php"); //importamos la conexion que se hizo global para AHORRAR LINEAS DE CODIGO
+    //llamamos el php que hace la conexion con la base de datos
+    include("../../../../Proyecto_SendApp_2024/bases/conexion.php");
     $conn = connection();
 
-    //chequeando la conexion
+    //comprueba si fallo la conexion
     if (!$conn) {
-        die("Conexion Fallo: ".mysqli_connect_error()); // Si hay error de conexion marca error
+        die("Conexion Fallo: ".mysqli_connect_error());
     }
+    //Esta parte es para admitir caracteres especiales
+    @mysqli_query($conn, "SET NAMES 'utf8'");
 
-    @mysqli_query($conn, "SET NAMES 'utf8'"); // Esta parte es para admitir caracteres en la base de datos
-
-    //Se hace una condicion pora que en caso tal de que algun campo de la validacion no se complete marque un error
+    //Condicional para verificar que los campos no estén vacíos, en caso de estarlos mandar un 1
     if ($_POST['documento_identidad'] == null || $_POST['contrasena'] == null){
         echo json_encode(array('success' => 1));
-        }else { //En caso tal de que todos los campos sean compleatados, lo que se ingreso sera guardado en una variable para luego hacer la validacion
+    } else {
+        //Obtener los datos de los input
         $documento_identidad = $_POST['documento_identidad'];
         $contrasena = $_POST['contrasena'];
         $estado = 1;
 
+        //Trae contraseña encriptada que coincida con el documento y la almacena en una variable 
         $resultado = mysqli_query($conn, "SELECT contrasena FROM usuarios WHERE documento_identidad = '$documento_identidad'");
         $row = mysqli_fetch_assoc($resultado);
         $contrasena_hash = $row['contrasena'];
 
-        // Verificar si la contraseña proporcionada coincide con el hash almacenado
-        if (password_verify($contrasena, $contrasena_hash)) {
-            // La contraseña es válida
-            // Establecer la sesión y redirigir según el rol del usuario
-            $consulta1 = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and contrasena = '$contrasena_hash' and estado = $estado and id_rol = (SELECT id_rol FROM roles WHERE nombre_rol = 'administrador') " ); //rol del administarador
-            $consulta2 = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and contrasena = '$contrasena_hash' and estado = $estado and id_rol = (SELECT id_rol FROM roles WHERE nombre_rol = 'funcionario') " ); //rol de funcionario
-            $consulta3 = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and contrasena = '$contrasena_hash' and estado = $estado and id_rol = (SELECT id_rol FROM roles WHERE nombre_rol = 'aprendiz') " ); //rol de aprendiz
-            $error_existencia = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and contrasena = '$contrasena_hash' and estado != $estado" ); // Error de estado inactivo
-
-            if (mysqli_fetch_array($consulta1)>0) {
+        //Verifica con el password_verify que la contraseña encriptada y la contraseña del input sean correctas
+        if (password_verify($contrasena, $contrasena_hash)){
+            //Consulta de administrador
+            $consulta1 = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and estado = $estado and id_rol = (SELECT id_rol FROM roles WHERE nombre_rol = 'administrador')");
+            //Verifica si hay resultados de la consulta y si es asi ejecuta lo que tiene dentro y termina la ejecución del PHP
+            if (mysqli_num_rows($consulta1) > 0) {
+                //Guarda el documento en la variable $_SESSION que puede ser utilizada en otros PHP
                 $_SESSION["documento_identidad"] = $documento_identidad;
+                //Manda una respuesta
                 echo json_encode(array('success' => 2));
-            } elseif (mysqli_fetch_array($consulta2)>0) {
+                //Termina ejecución
+                exit(); 
+            }
+
+            //Consulta funcionario
+            $consulta2 = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and estado = $estado and id_rol = (SELECT id_rol FROM roles WHERE nombre_rol = 'funcionario')");
+            if (mysqli_num_rows($consulta2) > 0) {
                 $_SESSION["documento_identidad"] = $documento_identidad;
                 echo json_encode(array('success' => 3));
-            } elseif (mysqli_fetch_array($consulta3)>0) {
-                $_SESSION["documento_identidad"] = $documento_identidad;
-                echo json_encode(array('success' => 6));
-            } elseif (mysqli_fetch_array($error_existencia)>0) {
-                echo json_encode(array('success' => 4));
-            } else{
-                echo json_encode(array('success' => 5));
+                exit(); 
             }
+
+            //Consulta aprendiz
+            $consulta3 = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and estado = $estado and id_rol = (SELECT id_rol FROM roles WHERE nombre_rol = 'aprendiz')");
+            if (mysqli_num_rows($consulta3) > 0) {
+                $_SESSION["documento_identidad"] = $documento_identidad;
+                echo json_encode(array('success' => 4));
+                exit(); 
+            }
+
+            //Consulta para verificar que el usuario este inactivo
+            $error_existencia = mysqli_query($conn, "SELECT * FROM usuarios WHERE documento_identidad = '$documento_identidad' and contrasena = '$contrasena_hash' and estado != $estado" );
+            if (mysqli_num_rows($consulta3) > 0) {
+                echo json_encode(array('success' => 5));
+                exit();
+            }
+        } else {
+            echo json_encode(array('success' => 6));
         }
-        mysqli_close($conn);
+        
+    mysqli_close($conn);
     }
