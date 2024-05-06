de8b1872f0ab5b79485a4005ccb65e6d8ce24be9
MiguelG <mg3503297@gmail.com>
2024-05-06 08:00:18 -0500

    Se cambio la barra de accesibilidad

--- scripts/ScriptsGenerales/accesibilidad.js
+++ scripts/ScriptsGenerales/accesibilidad.js
@@ -1,40 +1,29 @@
-//JS CONTRASTE
+document.addEventListener('DOMContentLoaded', function () {
+    const toggle = document.getElementById('toggle');
 
-let toggle=document.getElementById('toggle');
-toggle.addEventListener('change',(event)=>{
-    let checked=event.target.checked;
-    document.body.classList.toggle('dark')
+    // Verifica si hay un estado guardado en el almacenamiento local
+    let isDarkMode = localStorage.getItem('darkMode') === 'true';
 
-});
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Aplica el modo oscuro si estaba activado la última vez que se visitó la página
+    if (isDarkMode) {
+        document.body.classList.add('dark');
+        applyImageContrast();
+    }
 
-//JS SUBIR Y BAJAR
-
-document.getElementById('label_toggle4').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-// Bajar al final de la página
-document.getElementById('label_toggle5').addEventListener('click', function() {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-});
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-//JS ZOOM
-
-var zoomLevel = 100; // Zoom level in percentage
-
-    // Zoom in
-    document.getElementById('label_toggle2').addEventListener('click', function() {
-        // Zoom in effect
-        zoomLevel += 10;
-        document.body.style.zoom = zoomLevel + "%";
+    // Añade un event listener para detectar cambios en el estado del checkbox
+    toggle.addEventListener('change', function (event) {
+        let checked = event.target.checked;
+        document.body.classList.toggle('dark', checked);
+        // Guarda el estado en el almacenamiento local
+        localStorage.setItem('darkMode', checked);
+        applyImageContrast();
     });
 
-    // Zoom out
-    document.getElementById('label_toggle3').addEventListener('click', function() {
-        // Zoom out effect
-        zoomLevel -= 10;
-        document.body.style.zoom = zoomLevel + "%";
-    });
+    // Aplica el contraste a las imágenes si el modo oscuro está activado
+    function applyImageContrast() {
+        const images = document.querySelectorAll('img');
+        images.forEach(img => {
+            img.classList.toggle('inverted', document.body.classList.contains('dark'));
+        });
+    }
+});
