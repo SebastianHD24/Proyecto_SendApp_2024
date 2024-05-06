60fe44dcb9d8a3745e0e626d9647b7af68878637
GIT_Sebas <wowco.2000@gmail.com>
2024-05-06 11:27:58 -0500

    cambio de la base de datos, deben de importar la nueva base de datos

--- Data_Base/sendapp (desactualizada).sql
+++ Data_Base/sendapp (desactualizada).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-03-2024 a las 14:41:39
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Tiempo de generación: 25-04-2024 a las 16:01:05
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,6 +36,29 @@ CREATE TABLE `citas` (
   `id_servicio` int(2) NOT NULL COMMENT 'Muestra la id de los servicios'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`id_cita`, `estado_cita`, `fecha`, `hora`, `documento_usuario`, `id_servicio`) VALUES
+(1, 1, '2024-04-19', '0000-00-00 00:00:00', 1087986236, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pqr`
+--
+
+CREATE TABLE `pqr` (
+  `id_peticion` int(11) NOT NULL,
+  `documento_us` int(12) NOT NULL,
+  `tipo_pqrs` varchar(15) NOT NULL,
+  `descripcion` text NOT NULL,
+  `respuesta_pqrs` text DEFAULT NULL,
+  `fecha_solicitud` date DEFAULT NULL,
+  `fecha_respuesta` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -53,7 +76,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id_rol`, `nombre_rol`, `estado_rol`) VALUES
-(1, 'administrador', 1);
+(1, 'administrador', 1),
+(2, 'funcionario', 1),
+(3, 'aprendiz', 1);
 
 -- --------------------------------------------------------
 
@@ -68,6 +93,25 @@ CREATE TABLE `servicios` (
   `estado_servicio` tinyint(1) NOT NULL COMMENT 'Muestra el estado de los servicios'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id_servicio`, `nombre_servicio`, `admin_area`, `estado_servicio`) VALUES
+(1, 'biblioteca', 'sandra', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitud_citas`
+--
+
+CREATE TABLE `solicitud_citas` (
+  `id_solicitud_cita` int(11) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `id_usuario` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -75,19 +119,35 @@ CREATE TABLE `servicios` (
 --
 
 CREATE TABLE `usuarios` (
+  `tipo_documento` varchar(3) NOT NULL,
   `documento_identidad` int(12) NOT NULL COMMENT 'Es la identidad de la persona',
-  `contrasena` varchar(10) NOT NULL COMMENT 'Es la contrasena de todos los usuarios',
+  `contrasena` varchar(255) NOT NULL COMMENT 'Es la contrasena de todos los usuarios',
   `nombres` varchar(30) NOT NULL COMMENT 'Nombre del usuario\r\n',
   `apellidos` varchar(30) NOT NULL COMMENT 'Apellido del usuario',
   `correo` varchar(30) NOT NULL COMMENT 'Correo del usuario',
   `celular` varchar(15) NOT NULL COMMENT 'Celular del usuario',
   `programa` varchar(50) DEFAULT NULL COMMENT 'Programa del usuario',
   `ficha` int(10) DEFAULT NULL COMMENT 'Ficha del usuario',
-  `imagen_usuario` longblob NOT NULL,
   `estado` tinyint(1) NOT NULL COMMENT 'Estado del usuario',
   `id_rol` int(2) DEFAULT NULL COMMENT 'Rol del usuario',
-  `id_servicio` int(2) DEFAULT NULL COMMENT 'Los servicios del ususario'
+  `id_servicio` int(2) DEFAULT NULL COMMENT 'Los servicios del ususario',
+  `imagen` longblob DEFAULT NULL COMMENT 'Agrega tu foto o una imagen de perfil'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`tipo_documento`, `documento_identidad`, `contrasena`, `nombres`, `apellidos`, `correo`, `celular`, `programa`, `ficha`, `estado`, `id_rol`, `id_servicio`, `imagen`) VALUES
+('', 11111, '123', 'Camila', 'Quiceno', 'soymejor.@gmail.com', '3216716966', 'ADSO', 0, 1, 1, 1, NULL),
+('', 22222, '123', 'Jogo', 'Satoru', 'Jujutsukaisen.@gmail.com', '12345', '', 2618076, 1, 3, 1, NULL),
+('', 55555, '$2y$10$rhR', 'Steven', 'Cardenas', 'Steven@gmail.com', '1234321', '', 0, 1, 3, NULL, NULL),
+('', 99999, '123', 'ADMIN', 'ADMDIN', 'ADMIN@gmail.com', '123456789', NULL, NULL, 1, 1, NULL, NULL),
+('', 108888, '123', 'Yuely ', 'Arce Muñoz', 'yuelyAdriana@gmail.com', '3216739876', 'ADSO', 2618075, 1, 2, 1, NULL),
+('', 1231231, '123', 'Jhon', 'Itadori', 'soy.@gmail.com', '71273e2', '', 2618075, 1, 2, 1, NULL),
+('', 42146447, '$2y$10$aPi', 'Leidy', 'Ocampo', 'leidy@gmail.com', '3216716966', '', 0, 1, 3, NULL, NULL),
+('', 987654321, '123', 'santiago', 'Restrepo', 'lol.@gmail.com', '4321', '', 0, 1, 3, 1, NULL),
+('', 1087986236, '1234', 'sebastian', 'sanchez', 'wow@gmail.com', '3216735852', 'ADSO', 2618075, 1, 3, 1, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -102,6 +162,13 @@ ALTER TABLE `citas`
   ADD KEY `documento_usuario` (`documento_usuario`);
 
 --
+-- Indices de la tabla `pqr`
+--
+ALTER TABLE `pqr`
+  ADD PRIMARY KEY (`id_peticion`),
+  ADD KEY `documento_us` (`documento_us`);
+
+--
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
@@ -112,6 +179,13 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `servicios`
   ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- Indices de la tabla `solicitud_citas`
+--
+ALTER TABLE `solicitud_citas`
+  ADD PRIMARY KEY (`id_solicitud_cita`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -130,19 +204,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
-  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifica la id de la cita';
+  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifica la id de la cita', AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `pqr`
+--
+ALTER TABLE `pqr`
+  MODIFY `id_peticion` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id_rol` int(2) NOT NULL AUTO_INCREMENT COMMENT 'Visualiza el rol', AUTO_INCREMENT=2;
+  MODIFY `id_rol` int(2) NOT NULL AUTO_INCREMENT COMMENT 'Visualiza el rol', AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  MODIFY `id_servicio` int(2) NOT NULL AUTO_INCREMENT COMMENT 'Identifica los servicos del sena';
+  MODIFY `id_servicio` int(2) NOT NULL AUTO_INCREMENT COMMENT 'Identifica los servicos del sena', AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud_citas`
+--
+ALTER TABLE `solicitud_citas`
+  MODIFY `id_solicitud_cita` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -154,6 +240,18 @@ ALTER TABLE `servicios`
 ALTER TABLE `citas`
   ADD CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`documento_usuario`) REFERENCES `usuarios` (`documento_identidad`) ON UPDATE CASCADE,
   ADD CONSTRAINT `citas_ibfk_3` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pqr`
+--
+ALTER TABLE `pqr`
+  ADD CONSTRAINT `pqr_ibfk_1` FOREIGN KEY (`documento_us`) REFERENCES `usuarios` (`documento_identidad`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `solicitud_citas`
+--
+ALTER TABLE `solicitud_citas`
+  ADD CONSTRAINT `solicitud_citas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`documento_identidad`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuarios`
