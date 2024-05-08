06d2b623aa8674190d3ccd57bf80fb14eea52a23
Herllei <g2js06022003@gmail.com>
2024-05-08 12:15:55 -0500

    nada

--- interfaces/Usuario/actualizarVista.php
+++ interfaces/Usuario/actualizarVista.php
@@ -11,7 +11,8 @@
     $vista = 1;
 
     $sql = mysqli_query($conn, "UPDATE pqr SET vista = $vista WHERE documento_us = '$id_usuario'");
-    $consulta = "SELECT u.nombres, u.apellidos, u.id_rol, p.tipo_pqrs, p.respuesta_pqrs, p.fecha_solicitud, p.fecha_respuesta FROM pqr p INNER JOIN usuarios u ON p.documento_us = u.documento_identidad and u.documento_identidad = '$id_usuario'";
+    $consulta = "SELECT u.nombres, u.apellidos, u.id_rol, p.id_peticion, 
+    p.tipo_pqrs, p.respuesta_pqrs, p.fecha_solicitud, p.fecha_respuesta FROM pqr p INNER JOIN usuarios u ON p.documento_us = u.documento_identidad and u.documento_identidad = '$id_usuario'";
     $resultado = mysqli_query($conn, $consulta);
 
     if ($resultado->num_rows > 0){
