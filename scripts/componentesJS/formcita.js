a3a2551e9edb32c265af1fed8305ee0c45770bd5
juanes1802 <menajuanes945@gmail.com>
2024-05-09 11:22:18 -0500

    la bolas ya manda el formualrio y el formularia se envia a guardarcita y luego se puede visualizar

--- scripts/componentesJS/formcita.js
+++ scripts/componentesJS/formcita.js
@@ -1,9 +1,16 @@
 let buttonBiblioteca = document.getElementById('biblioteca'),
-buttonBienestar = document.getElementById('Bienestar');
+ buttonBienestar = document.getElementById('Bienestar'),
+ buttonFondoEmprender = document.getElementById('fondo_emprender'),
+ buttonRelacionesCorporativas = document.getElementById('relaciones_Corporativas'),
+ buttonSennova = document.getElementById('sennova'),
+ buttonServiciosTecnologicos = document.getElementById('Servicios_tecnologicos'),
+ buttonFabricaSoftware = document.getElementById('fabrica_Software'),
+ buttonDeportes = document.getElementById('deportes'),
+ buttonPsicologia = document.getElementById('psicologia');
 let contenedor = document.querySelector('.container');
 let div_container = document.querySelector('.div__content');
 // Obtener referencias a los botones
-const botones = [buttonBienestar, buttonBiblioteca];
+const botones = [buttonBienestar, buttonBiblioteca, buttonPsicologia,buttonFondoEmprender,buttonRelacionesCorporativas,buttonSennova,buttonServiciosTecnologicos,buttonFabricaSoftware,buttonDeportes];
 
 // Definir el evento
 function mostrarContenedor() {
