c4fd6f0c34cd6b341bf328fdb78c92c0d179cfba
Hideoshi <narazaky0307@gmail.com>
2024-05-11 17:55:22 -0500

    Cristian David: Calendario de citas

---
+++ bases/mainInterfaz/backend/obtener_notificaciones.php
@@ -0,0 +1,35 @@
+<?php
+
+//Incluimos la sesion iniciada, la conexion a la base de datos
+include '../../../../Proyecto_SendApp_2024/bases/conexion.php';
+
+// Obtener la conexión a la base de datos
+$conn = connection();
+
+// Obtener la fecha del POST
+$data = json_decode(file_get_contents("php://input"));
+$day = $data->dia;
+$month = $data->mes;
+$year = $data->anio;
+
+// Formatear la fecha para que coincida con el formato en la base de datos (YYYY-MM-DD)
+$fecha = sprintf("%04d-%02d-%02d", $year, $month, $day);
+
+// Realizar la consulta SQL para verificar si hay citas para la fecha proporcionada
+$query = "SELECT * FROM citas WHERE fecha = '$fecha'";
+$result = mysqli_query($conn, $query);
+
+// Verificar si hay resultados
+if(mysqli_num_rows($result) > 0) {
+    // Si hay citas, devolver "si" en JSON
+    $response = array('respuesta' => 'si');
+} else {
+    // Si no hay citas, devolver "no" en JSON
+    $response = array('respuesta' => 'no');
+}
+
+// Devolver la respuesta en formato JSON
+echo json_encode($response);
+?>
+
+
