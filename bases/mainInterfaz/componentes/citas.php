7b3e86bdd6bce4296ef22257a676353b33ecec18
Juanes <menajuanes945@gmail.com>
2024-05-23 21:10:55 -0500

    actualizando

--- bases/mainInterfaz/componentes/citas.php
+++ bases/mainInterfaz/componentes/citas.php
@@ -37,19 +37,13 @@ LIMIT 6";
                 ?>
                 <!--Tarjeta donde se muestra la información de la cita-->
                 <div class="notifications">
-<<<<<<< HEAD
                 <div  id="justificacion-rechazo-<?= $row['id_cita'] ?>" class="popup-justificacion" style="display:none;
                       " >
                         <p><?= $row['justificacion_rechazo']?> </p>
                     </div>
                     <figure>
-=======
-                    <!--Contenedor del logo superior-->
-                    <figure> 
->>>>>>> 8d0ff1a635315c7a7ea6eefb367ca9bdf12dc390
                         <img src="../../../../Proyecto_SendApp_2024/imagenes/Componentes-img/Schedule.png" class="notifications-logo" alt="Icono de Calendario"/>
                     </figure>
-<<<<<<< HEAD
                     <span></span>
                     <article>
                         <b>Área: </b>
@@ -110,77 +104,6 @@ LIMIT 6";
                 <?php
             }
             ?>
-=======
-                        <!-- Barras que separadao de contenido en la tarjeta -->
-                        <span></span>
-                            <!-- Artículo contenedor de datos extraido de la BD -->
-                            <article>
-                                <b>
-                                    Área: <!--Dato Descriptivo -->
-                                </b> 
-                                <p>
-                                    <?= $row['nombre_servicio'] ?> <!--Dato extraido de la BD -->
-                                </p> 
-                            </article> 
-                                <!-- Barras que separadao de contenido en la tarjeta -->
-                                <span></span>
-                                    <article>
-                                        <b>
-                                            Día: 
-                                        </b>
-                                        <p>
-                                            <?= empty($row['fecha']) ? "Aún no te han asignado el dia" : $row['fecha'] ?>
-                                        </p>
-                                    </article>
-                                        <!-- Barras que separadao de contenido en la tarjeta -->
-                                        <span></span>
-                                            <article>
-                                                <b>
-                                                    Hora: 
-                                                </b>
-                                                <p> 
-                                                    <?= empty($row['hora']) ? "Aún no te han asignado hora" : $row['hora'] ?> 
-                                                </p>
-                                            </article>
-                                                <!-- Barras que separadao de contenido en la tarjeta -->
-                                                <span></span>
-                                                    <article>
-                                                        <b>
-                                                            Estado: 
-                                                        </b>
-                                                        <b class="estado_cita">
-                                                            <?= $row['estado_cita'] ?>
-                                                        </b>
-                                                    </article>
-                                                <!-- Barras que separadao de contenido en la tarjeta -->
-                                                <span></span>
-                                            <article>
-                                                <b>Jornada: </b>
-                                                <p>
-                                                    <?= $row['jornada'] ?>
-                                                </p>
-                                            </article>
-                                        <!-- Barras que separadao de contenido en la tarjeta -->
-                                        <span></span>
-                                    <article>
-                                        <b>Funcionario: </b>
-                                        <p>
-                                            <?= $row['nombre_funcionario_cita'] . ' ' . $row['apellido_funcionario_cita'] ?>
-                                        </p>
-                                    </article>
-                                <!-- Barras que separadao de contenido en la tarjeta -->
-                                <span></span>
-                            <article>
-                                <b>Motivo: </b>
-                                <p> 
-                                    <?= $row['descripcion'] ?>
-                                </p>
-                            </article>
-                        </div>
-                    <?php
-                    }
-                ?>
->>>>>>> 8d0ff1a635315c7a7ea6eefb367ca9bdf12dc390
             </div> <!-- Aquí se cierra el único contenedor de cuadrícula -->
         <?php
     } else {
