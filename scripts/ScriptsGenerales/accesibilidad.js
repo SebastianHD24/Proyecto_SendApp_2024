931cab9be649f32f5b523ee6afe47808fa45668c
MiguelG <mg3503297@gmail.com>
2024-06-01 16:32:42 -0500

    Se modifico el logo de inicio cuando el dark se active

--- scripts/ScriptsGenerales/accesibilidad.js
+++ scripts/ScriptsGenerales/accesibilidad.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.getElementById('toggle');
+    const logos = document.querySelectorAll('.logo');
 
     // Verifica si hay un estado guardado en el almacenamiento local
     let isDarkMode = localStorage.getItem('darkMode') === 'true';
@@ -7,13 +8,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // Aplica el modo oscuro si estaba activado la última vez que se visitó la página
     if (isDarkMode) {
         document.body.classList.add('dark');
+        logos.forEach(logo => {
+            logo.src = '../Inicio/Img-home/LogosSena-img/logoSendappDark2.png'
+        });
+        toggle.checked = true;
     }
 
     // Añade un event listener para detectar cambios en el estado del checkbox
     toggle.addEventListener('change', function (event) {
         let checked = event.target.checked;
         document.body.classList.toggle('dark', checked);
-        // Guarda el estado en el almacenamiento local
+        logos.forEach(logo => {
+            logo.src = checked ? '../Inicio/Img-home/LogosSena-img/logoSendappDark2.png': '../Inicio/Img-home/LogosSena-img/logoSendappDark2.png'
+        })
         localStorage.setItem('darkMode', checked);
     });
 });
