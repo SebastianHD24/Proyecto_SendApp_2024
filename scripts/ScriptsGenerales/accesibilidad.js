e7bd41b5d4bd5a4a698142a68e73ac6526204fae
MiguelG <mg3503297@gmail.com>
2024-06-01 16:42:58 -0500

    Se implemento el fondo del header para cuando se haga dark

--- scripts/ScriptsGenerales/accesibilidad.js
+++ scripts/ScriptsGenerales/accesibilidad.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.getElementById('toggle');
-    const logos = document.querySelectorAll('.logo');
 
     // Verifica si hay un estado guardado en el almacenamiento local
     let isDarkMode = localStorage.getItem('darkMode') === 'true';
@@ -8,9 +7,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Aplica el modo oscuro si estaba activado la última vez que se visitó la página
     if (isDarkMode) {
         document.body.classList.add('dark');
-        logos.forEach(logo => {
-            logo.src = '../Inicio/Img-home/LogosSena-img/logoSendappDark2.png'
-        });
         toggle.checked = true;
     }
 
@@ -18,9 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
     toggle.addEventListener('change', function (event) {
         let checked = event.target.checked;
         document.body.classList.toggle('dark', checked);
-        logos.forEach(logo => {
-            logo.src = checked ? '../Inicio/Img-home/LogosSena-img/logoSendappDark2.png': '../Inicio/Img-home/LogosSena-img/logoSendappDark2.png'
-        })
         localStorage.setItem('darkMode', checked);
     });
 });
