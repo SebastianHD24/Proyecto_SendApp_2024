91ae0daaa9a84714f07e0dacb8410d4fbb62c1bd
Herllei <g2js06022003@gmail.com>
2024-06-27 08:45:36 -0500

    arreglo de la contraseña y el nombre de las PQR

--- interfaces/Usuario/Scripts/mostrarHistorial.js
+++ interfaces/Usuario/Scripts/mostrarHistorial.js
@@ -33,7 +33,7 @@ function createNotificationBoxH() {
     let textArticle = document.createElement("article");
     textArticle.classList.add("article__text--1");
     let textParagraph = document.createElement("p");
-    textParagraph.textContent = "Atención a su solicitud de QPR.";
+    textParagraph.textContent = "Atención a su solicitud de PQR.";
     textArticle.appendChild(textParagraph);
     notificationsPqr.appendChild(textArticle);
 
